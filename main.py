ad5f763c64a86a842bebb463dd52ae99e5148b97
mrmichaelkang <mrmichaelkang@gmail.com>
2025-06-20 11:56:17 -0700

    created logic for moving player

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("PyGame - Asteroids")
     fps = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updateable.add(player)
+    drawable.add(player)
 
     while True:
         for event in pygame.event.get():
@@ -16,10 +23,14 @@ def main():
                 return
 
         screen.fill(pygame.Color(0, 0, 0))
-        # pygame.display.flip()
-        player.draw(screen)
-
         dt = fps.tick(60) / 1000
+
+        for item in updateable:
+            item.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
 
