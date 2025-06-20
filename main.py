90c73c1345055ec967cb8713aee4e9c909949237
mrmichaelkang <mrmichaelkang@gmail.com>
2025-06-20 08:56:04 -0700

    created player

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill(pygame.Color(0, 0, 0))
+        # pygame.display.flip()
+        player.draw(screen)
+
+        dt = fps.tick(60) / 1000
         pygame.display.flip()
 
 
