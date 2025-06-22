2c6215d9d47f01712608b82fa23190434303ce48
mrmichaelkang <mrmichaelkang@gmail.com>
2025-06-22 08:17:00 -0700

    created asteroids for game

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import circleshape
+import pygame
+
+
+class Asteroid(circleshape.CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        return pygame.draw.circle(screen, (255, 255, 255), self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -9,13 +11,17 @@ def main():
     pygame.display.set_caption("PyGame - Asteroids")
     fps = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    asteroids = pygame.sprite.Group()
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
-    updateable.add(player)
-    drawable.add(player)
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -23,15 +29,14 @@ def main():
                 return
 
         screen.fill(pygame.Color(0, 0, 0))
-        dt = fps.tick(60) / 1000
 
-        for item in updateable:
-            item.update(dt)
+        updateable.update(dt)
 
         for item in drawable:
             item.draw(screen)
 
         pygame.display.flip()
+        dt = fps.tick(60) / 1000
 
 
 if __name__ == "__main__":
